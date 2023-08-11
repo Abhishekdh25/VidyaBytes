d04342941e0ecc72848a6213bfe2f81678a94b7d
Abhishekdh25 <97876385+Abhishekdh25@users.noreply.github.com>
2023-08-11 14:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 $(document).ready(function() {
-    const cardsContainer = $(".cards");
-    const numVisibleCards = 2;
-    let currentIndex = 0;
-    let cardWidth;
-  
-    // Function to recalculate card width on window resize
-    function recalculateCardWidth() {
-      const sliderContainerWidth = $(".slider-container").width();
-      cardWidth = sliderContainerWidth / numVisibleCards;
-      $(".card").width(cardWidth);
-    }
-  
-    // Call the recalculateCardWidth function on document ready and window resize
-    recalculateCardWidth();
-    $(window).on("resize", recalculateCardWidth);
-  
-    $(".slider-container").swipe({
-      swipeLeft: function() {
-        if (currentIndex < cardsContainer.children().length - numVisibleCards) {
-          currentIndex++;
-          updateSliderPosition();
-        }
-      },
-      swipeRight: function() {
-        if (currentIndex > 0) {
-          currentIndex--;
-          updateSliderPosition();
-        }
-      },
-      preventDefaultEvents: false,
-    });
-  
-    function updateSliderPosition() {
-      const offset = -currentIndex * cardWidth;
-      cardsContainer.css("transform", `translateX(${offset}px)`);
-    }
+  const cardsContainer = $(".cards");
+  const numVisibleCards = 2;
+  const cardWidth = 150; // Fixed width for the cards
+  const cardHeight = 150; // Fixed height for the cards
+  let currentIndex = 0;
+
+  $(".card").css({
+    width: cardWidth + "px",
+    height: cardHeight + "px",
   });
+
+  $(".slider-container").swipe({
+    swipeLeft: function() {
+      if (currentIndex < cardsContainer.children().length - numVisibleCards) {
+        currentIndex++;
+        updateSliderPosition();
+      }
+    },
+    swipeRight: function() {
+      if (currentIndex > 0) {
+        currentIndex--;
+        updateSliderPosition();
+      }
+    },
+    preventDefaultEvents: false,
+  });
+
+  function updateSliderPosition() {
+    const offset = -currentIndex * cardWidth;
+    cardsContainer.css("transform", `translateX(${offset}px)`);
+  }
+});
